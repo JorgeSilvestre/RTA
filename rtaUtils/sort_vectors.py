36886b968ddfdb430db29db165a9f2b24bc9e833
jsilvestre <35924387+JorgeSilvestre@users.noreply.github.com>
2023-03-30 15:37:59 +0200

    Update sort_vectors, add viz dashboard
    Fixed distance function with angular variation using track value
    Added a dashboard to visualize the results of sorting trajectories
    Added method to identify track oscillation

--- rtaUtils/sort_vectors.py
+++ rtaUtils/sort_vectors.py
@@ -23,8 +23,9 @@ TMA_AREA_MAX = 110
 TMA_AREA_MIN = 30
 AIRPORT_AREA = 5
 
-MAX_ROTATION = 365
-SINGLE_LOOP_ROTATION = 180
+HOLDING_ROTATION = 365
+LOOP_ROTATION = 180
+MIN_OSCILLATION = 10
 
 # Parámetros del proceso
 overlap_fast     = 5
@@ -40,13 +41,13 @@ max_iteraciones = 100
 def calculate_distance(array: np.array, angle = False) -> np.array:
     array = array.astype('float32')
 
-    distances = haversine_np(array[1:,0], array[1:,1],
-                             array[:-1,0], array[:-1,1],
-                             None, None, angle)
-    
-    # distances = haversine_np_track(array[1:,0], array[1:,1],
+    # distances = haversine_np(array[1:,0], array[1:,1],
     #                          array[:-1,0], array[:-1,1],
-    #                          array[1:,2], array[:-1,2], angle)
+    #                          None, None, angle)
+    
+    distances = haversine_np_track(array[1:,0], array[1:,1],
+                             array[:-1,0], array[:-1,1],
+                             array[1:,2], array[:-1,2], angle)
 
     return np.concatenate([[0], distances])
 
@@ -130,14 +131,54 @@ def sort_windows(array: np.array, windows: tuple, angle: bool = False) -> np.arr
 
 
 def calculate_rotation(tracks: pd.Series) -> float:
+    if tracks.shape[0] == 0:
+        return 0
+    
     # Rotación = Σ Variación de track entre mensajes adyacentes
     delta = tracks.astype(int).rolling(2,2).apply(lambda x: x.iloc[1]-x.iloc[0])
     delta = delta.apply(lambda x: x if abs(x) < 180 else (x - np.sign(x)*360))
     # Primer elemento es nan
-    track_variation = delta.iloc[1:].sum()
+    # track_variation = delta.iloc[1:].sum()
+    track_variation = delta.iloc[1:].cumsum()
+    track_variation = track_variation.iloc[track_variation.abs().argmax()]
 
     return track_variation
 
+
+def detect_rotation_oscillations(data: pd.Series) -> int:
+    # https://stackoverflow.com/a/64747209
+    if data.shape[0] == 0:
+        return 0
+    
+    lat1, lat2 = data.latitude.values[:-1], data.latitude.values[1:]
+    lon1, lon2 = data.longitude.values[:-1], data.longitude.values[1:]
+    dlon = lon2-lon1
+
+    x = np.cos(np.radians(lat2)) * np.sin(np.radians(dlon))
+    y = np.cos(np.radians(lat1)) * np.sin(np.radians(lat2)) - np.sin(np.radians(lat1)) * np.cos(np.radians(lat2)) * np.cos(np.radians(dlon))
+    angles = np.arctan2(x,y)
+    angles = np.degrees(angles) % 360
+    angles = np.round(angles, 0)
+
+    # print(angles)
+    # print(data.track.values[1:])
+
+    return (np.abs((angles - data.track.values[1:]))>MIN_OSCILLATION).sum()/angles.shape[0]
+
+
+def detect_rotation_oscillations2(tracks: pd.Series) -> int:
+    if tracks.shape[0] == 0:
+        return 0
+    
+    delta = tracks.astype(int).rolling(2,2).apply(lambda x: x.iloc[1]-x.iloc[0])
+    delta = delta.apply(lambda x: x if abs(x) < 180 else (x - np.sign(x)*360))
+
+    delta[delta.abs()<=MIN_OSCILLATION] = 0
+
+    delta = delta.rolling(2,2).apply(lambda x: np.sign(x.iloc[0])!=np.sign(x.iloc[1]))
+
+    return delta.sum()/tracks.shape[0]
+    
 
 def sort_nearest_vector(array: np.array, stop: str = 'undefined') -> np.array:
     '''
@@ -177,123 +218,6 @@ def sort_nearest_vector(array: np.array, stop: str = 'undefined') -> np.array:
     unsorted = array[mask,:]
 
     return np.concatenate([sorted,unsorted], axis=0)
-
-
-def sort_by_position(data: pd.DataFrame, remove_ground_vectors: bool = False) -> tuple[pd.DataFrame, float]:
-    '''
-        Ordena las filas de un dataframe de acuerdo a su latitud y longitud. Mantiene el
-        índice original (el orden anterior queda registrado en la columna ordenInicial)
-
-        data : pd.DataFrame
-            Dataframe con los vectores ordenados por timestamp
-        remove_ground_vectors : bool
-            Indica si deben eliminarse los vectores en tierra en el aeropuerto de origen
-    '''
-    if data.shape[0] == 0:
-        return data
-
-    # Investigar estos duplicados
-    # Dan lugar a error en el cálculo de las distancias
-    data = data[~data.duplicated(subset=['latitude'])]
-    data = data[~data.duplicated(subset=['longitude'])]
-
-    stats = {}
-    last_vector = None
-
-    origin_airport = airports[airports.id == data.iloc[0].aerodromeOfDeparture].iloc[0]
-    data['distance_org'] = haversine_np(data.latitude, data.longitude,
-                                        origin_airport.lat, origin_airport.lon)
-    data['distance_dst'] = haversine_np(data.latitude, data.longitude)
-
-    if remove_ground_vectors:
-        # Eliminamos mensajes en tierra en aeropuerto de origen
-        st = data.shape[0]
-        data = data[~(data.ground & (data.distance_org < TMA_AREA_MIN))].copy()
-        stats['dropped_ground'] = st - data.shape[0]
-
-        # min_index = data.index.min()
-        # data = data.reset_index(drop=True)
-        # data.index += min_index
-
-    # Si hay vectores en origen, tomamos el primero cronológicamente
-    if data[data.distance_org < AIRPORT_AREA].shape[0]:
-        data['distance_org'] = haversine_np(data.latitude, data.longitude,
-                                            data.iloc[0].latitude, data.iloc[0].longitude)
-
-    # Si hay vectores en destino, tomamos como último el más cercano al punto de referencia
-    # if data[data.distance_dst < AIRPORT_AREA].shape[0]:
-    #     last_vector = data.loc[data[data.distance_dst < AIRPORT_AREA].distance_dst.idxmin()]
-
-    # Segments identification
-    end_origin_segment = data[data.distance_org<TMA_AREA_MAX/2].shape[0]
-    end_cruise_segment = data[data.distance_dst>TMA_AREA_MAX].shape[0]
-    stats['initial'] = calculate_distance(data[['latitude','longitude','track']].values).sum()
-
-    data['ordenInicial'] = range(data.shape[0])
-
-    # Aseguramos que los primeros mensajes son los más cercanos al aeropuerto de origen
-    data.iloc[:] = data.iloc[:].sort_values(by='distance_org').values
-    # Tramo de salida ordenado por timestamp
-    # data.iloc[:end_origin_segment] = data.iloc[:end_origin_segment].sort_values(by='timestamp').values
-    data.iloc[:end_origin_segment] = sort_nearest_vector(data.iloc[:end_origin_segment].values)
-    # Tramo de crucero ordenado por distancia decreciente al aeropuerto de destino
-    data.iloc[end_origin_segment:end_cruise_segment] = data.iloc[end_origin_segment:end_cruise_segment].sort_values(by='distance_dst', ascending=False).values
-    # Tramo de entrada ordenado por timestamp
-    # data.iloc[end_cruise_segment:] = data.iloc[end_cruise_segment:].sort_values(by='timestamp').values
-    # Tramo de entrada preordenado de acuerdo al vector más cercano
-    # data.iloc[end_cruise_segment:] = presort(data.iloc[end_cruise_segment:].values, last_vector.vectorId if not (last_vector is None) else data.shape[0])
-    data.iloc[end_cruise_segment:] = sort_nearest_vector(data.iloc[end_cruise_segment:].values)
-
-    # Rotación del avión en zona de maniobras
-    tmp = data[(data.distance_dst.between(TMA_AREA_MIN, TMA_AREA_MAX)) & (data.altitude>0)].copy()
-    track_variation = calculate_rotation(tmp.track)
-
-    # data = data.drop(['distance_org', 'distance_dst'], axis=1)
-
-    # Primer tramo
-    windows = generate_windows(data.shape[0], window_size_slow, overlap_slow, 0, end_origin_segment)
-    data.iloc[:] = sort_windows(data.values, windows, angle=False)
-    # Segundo tramo
-    windows = generate_windows(data.shape[0], window_size_fast, overlap_fast, end_origin_segment, end_cruise_segment)
-    data.iloc[:] = sort_windows(data.values, windows, angle=False)
-
-    # Para descartar reordenación del tercer tramo si el resultado es peor que el inicial
-    # by_time = data.copy()
-    # by_time.iloc[end_cruise_segment:] = by_time.iloc[end_cruise_segment:].sort_values(by='timestamp').values
-    # by_nearest = data.copy()
-
-    # Tercer tramo
-    last_idx=data.shape[0]
-    # last_idx = data.index.get_loc(data[data.vectorId == last_vector.vectorId].iloc[0].name) if not (last_vector is None) else data.shape[0]
-    windows = generate_windows(data.shape[0], window_size_slow, overlap_slow, end_cruise_segment, last_idx) #-2
-    if abs(track_variation) > MAX_ROTATION:
-        # print(f'WARNING: Loop múltiple detectado en {data.fpId.iloc[0]}')
-        stats['loop'] = 'D'
-        data.iloc[:] = sort_windows(data.values, windows, angle=True)
-    elif abs(track_variation) > SINGLE_LOOP_ROTATION:
-        stats['loop'] = 'S'
-        data.iloc[:] = sort_windows(data.values, windows, angle=True)
-    else:
-        data.iloc[:] = sort_windows(data.values, windows, angle=False)
-
-    data['ordenFinal'] = range(data.shape[0]) # data.index #
-
-    final_distance = calculate_distance(data[['latitude','longitude','track']].values).sum()
-    stats['final'] = final_distance
-
-    # time_distance=covered_distance(by_time[['latitude','longitude','track']].values).sum()
-    # time_presort=covered_distance(by_nearest[['latitude','longitude','track']].values).sum()
-    # print(f"{data.fpId.iloc[0]}     Time: {time_distance:8.2f}     \
-    #       Pres: {time_presort:8.2f} ({100*(time_presort-time_distance)/time_distance:5.1f}%)     \
-    #       TSP: {final_distance:8.2f} ({100*(final_distance-time_distance)/time_distance:5.1f}%)")
-
-    # if  time_distance < final_distance:
-    #     data = by_time
-    #     stats['final'] = time_distance
-
-    stats['rotation'] = track_variation
-
-    return data, stats
 
 
 def is_resorted(x, acc_list: list) -> bool:
@@ -401,8 +325,130 @@ def fix_altitude(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def fix_trajectory(data: pd.DataFrame, remove_ground_vectors: bool = True) -> pd.DataFrame:
+def sort_by_position(data: pd.DataFrame, remove_ground_vectors: bool = False) -> tuple[pd.DataFrame, float]:
+    '''
+        Ordena las filas de un dataframe de acuerdo a su latitud y longitud. Mantiene el
+        índice original (el orden anterior queda registrado en la columna ordenInicial)
 
+        data : pd.DataFrame
+            Dataframe con los vectores ordenados por timestamp
+        remove_ground_vectors : bool
+            Indica si deben eliminarse los vectores en tierra en el aeropuerto de origen
+    '''
+    if data.shape[0] == 0:
+        return data
+
+    # Investigar estos duplicados
+    # Dan lugar a error en el cálculo de las distancias
+    data = data[~data.duplicated(subset=['latitude'])]
+    data = data[~data.duplicated(subset=['longitude'])]
+
+    stats = {}
+    last_vector = None
+
+    #################### Origin airport ####################
+    origin_airport = airports[airports.id == data.iloc[0].aerodromeOfDeparture].iloc[0]
+    data['distance_org'] = haversine_np(data.latitude, data.longitude,
+                                        origin_airport.lat, origin_airport.lon)
+    data['distance_dst'] = haversine_np(data.latitude, data.longitude)
+
+    if remove_ground_vectors:
+        # Eliminamos mensajes en tierra en aeropuerto de origen
+        st = data.shape[0]
+        data = data[~(data.ground & (data.distance_org < TMA_AREA_MIN))].copy()
+        stats['dropped_ground'] = st - data.shape[0]
+
+    # Si hay vectores en origen, tomamos el primero cronológicamente
+    if data[data.distance_org < AIRPORT_AREA].shape[0]:
+        data['distance_org'] = haversine_np(data.latitude, data.longitude,
+                                            data.iloc[0].latitude, data.iloc[0].longitude)
+
+    #################### Destination airport ####################
+    # Si hay vectores en destino, tomamos como último el más cercano al punto de referencia
+    # if data[data.distance_dst < AIRPORT_AREA].shape[0]:
+    #     last_vector = data.loc[data[data.distance_dst < AIRPORT_AREA].distance_dst.idxmin()]
+
+    data['ordenInicial'] = range(data.shape[0])
+
+    #################### Presort ####################
+
+    # Segments identification
+    end_origin_segment = data[data.distance_org<TMA_AREA_MAX/2].shape[0]
+    end_cruise_segment = data[data.distance_dst>TMA_AREA_MAX].shape[0]
+    stats['initial'] = calculate_distance(data[['latitude','longitude','track']].values).sum()
+
+    # Aseguramos que los primeros mensajes son los más cercanos al aeropuerto de origen
+    data.iloc[:] = data.iloc[:].sort_values(by='distance_org').values
+    # Segmento de salida ordenado por NV
+    data.iloc[:end_origin_segment] = sort_nearest_vector(data.iloc[:end_origin_segment].values)
+    # Segmento de crucero ordenado por distancia decreciente al aeropuerto de destino
+    data.iloc[end_origin_segment:end_cruise_segment] = data.iloc[end_origin_segment:end_cruise_segment].sort_values(by='distance_dst', ascending=False).values
+    # Segmento de entrada ordenado por timestamp
+    # data.iloc[end_cruise_segment:] = data.iloc[end_cruise_segment:].sort_values(by='timestamp').values
+    # Segmento de entrada ordenado por NV
+    data.iloc[end_cruise_segment:] = sort_nearest_vector(data.iloc[end_cruise_segment:].values)
+
+    # Oscilaciones en el track
+    tmp = data[(data.distance_dst.between(TMA_AREA_MAX, TMA_AREA_MAX+200))]
+    stats['oscillation'] = detect_rotation_oscillations(tmp[['latitude','longitude','track']])
+    stats['oscillation_sample'] = tmp.shape[0]
+
+    # Rotación del avión en zona de maniobras
+    tmp = data[(data.distance_dst.between(TMA_AREA_MIN, TMA_AREA_MAX)) & (data.altitude>0)].copy()
+    track_variation = calculate_rotation(tmp.track)
+    stats['rotation'] = track_variation
+
+    # data = data.drop(['distance_org', 'distance_dst'], axis=1)
+
+    #################### TSP sorting ####################
+
+    # Primer tramo
+    windows = generate_windows(data.shape[0], window_size_slow, 
+                               overlap_slow, 0, end_origin_segment)
+    data.iloc[:] = sort_windows(data.values, windows, angle=False)
+    # Segundo tramo
+    windows = generate_windows(data.shape[0], window_size_fast, 
+                               overlap_fast, end_origin_segment, end_cruise_segment)
+    data.iloc[:] = sort_windows(data.values, windows, angle=False)
+
+    # Para descartar reordenación del tercer tramo si el resultado es peor que el inicial
+    # by_time = data.copy()
+    # by_time.iloc[end_cruise_segment:] = by_time.iloc[end_cruise_segment:].sort_values(by='timestamp').values
+    # by_nearest = data.copy()
+
+    # Tercer tramo
+    last_idx=data.shape[0]
+    # last_idx = data.index.get_loc(data[data.vectorId == last_vector.vectorId].iloc[0].name) if not (last_vector is None) else data.shape[0]
+    windows = generate_windows(data.shape[0], window_size_slow, 
+                               overlap_slow, end_cruise_segment, last_idx) 
+    if abs(track_variation) > HOLDING_ROTATION:
+        stats['loop'] = 'D'
+        data.iloc[:] = sort_windows(data.values, windows, angle=True)
+    elif abs(track_variation) > LOOP_ROTATION:
+        stats['loop'] = 'S'
+        data.iloc[:] = sort_windows(data.values, windows, angle=True)
+    else:
+        data.iloc[:] = sort_windows(data.values, windows, angle=False)
+
+    data['ordenFinal'] = range(data.shape[0])
+
+    final_distance = calculate_distance(data[['latitude','longitude','track']].values).sum()
+    stats['final'] = final_distance
+
+    # time_distance=covered_distance(by_time[['latitude','longitude','track']].values).sum()
+    # time_presort=covered_distance(by_nearest[['latitude','longitude','track']].values).sum()
+    # print(f"{data.fpId.iloc[0]}     Time: {time_distance:8.2f}     \
+    #       Pres: {time_presort:8.2f} ({100*(time_presort-time_distance)/time_distance:5.1f}%)     \
+    #       TSP: {final_distance:8.2f} ({100*(final_distance-time_distance)/time_distance:5.1f}%)")
+
+    # if  time_distance < final_distance:
+    #     data = by_time
+    #     stats['final'] = time_distance
+
+    return data, stats
+
+
+def fix_trajectory(data: pd.DataFrame, remove_ground_vectors: bool = True) -> pd.DataFrame:
     # time1 = time.time()
     data, stats = sort_by_position(data, remove_ground_vectors=remove_ground_vectors)
     # time1 = time.time() - time1
@@ -416,15 +462,17 @@ def fix_trajectory(data: pd.DataFrame, remove_ground_vectors: bool = True) -> pd
     
     initial = stats.get('initial', -1)
     final = stats.get('final', -1)
+    oscillation = stats.get('oscillation', -1)
+    oscillation_sample = stats.get('oscillation_sample', -1)
     rotation = stats.get('rotation', -1)
     dropped = stats.get('dropped_ground', -1)
     loop = stats.get('loop', " ")
     
-    print(f'{data.fpId.iloc[0]}: {dropped:3} {int(rotation):>4}{loop}  {initial:9.2f} -> {final:8.2f} ({((final-initial)/initial)*100:6.2f}%)')
+    print(f'{data.fpId.iloc[0]}({data.shape[0]:>4}): {dropped:3} {oscillation*100:>3.0f}% ({oscillation_sample:>3}) {int(rotation):>4}{loop}  {initial:9.2f} -> {final:8.2f} ({((final-initial)/initial)*100:6.2f}%)')
     # print(f"Sort: {time1:>2.2f}s   Timestamp: {time2:>2.2f}s   Altitude: {time3:>2.2f}s")
 
     with open('sort_stats.csv', 'a+', encoding='utf8') as file:
-        file.write(f'{int(time.time())},{data.flightDate.iloc[0]},{data.fpId.iloc[0]},{dropped},{rotation},{initial},{final},{((final-initial)/initial)*100:.2f}\n')
+        file.write(f'{int(time.time())},{data.flightDate.iloc[0]},{data.fpId.iloc[0]},{data.shape[0]},{dropped},{oscillation},{oscillation_sample},{rotation},{initial},{final},{((final-initial)/initial)*100:.2f}\n')
 
     return data
         #[['vectorId', 'fpId', 'aerodromeOfDeparture', 'latitude', 'longitude',
@@ -433,7 +481,7 @@ def fix_trajectory(data: pd.DataFrame, remove_ground_vectors: bool = True) -> pd
 
 
 def main():
-    date_start, date_end = '2022-09-21', '2022-09-21'
+    date_start, date_end = '2022-09-21', '2022-09-22'
 
     date_start = datetime.datetime.strptime(date_start, '%Y-%m-%d')
     date_end   = datetime.datetime.strptime(date_end,   '%Y-%m-%d')
@@ -447,8 +495,7 @@ def main():
         try:
             flights = data_loading.load_raw_data_sort(date)
             # flights = pd.read_parquet('../data/test.parquet')
-
-            # flights = flights[flights.fpId==flights.fpId.unique()[3]] ### TEST  AT05521006 AT05516307
+            # flights = flights[flights.fpId=='AT05509259'] ### TEST  AT05521006 AT05516307
         except IndexError: # Si no hay datos para el día
             continue
         indices = data_loading.calculate_indexes(flights)
@@ -460,9 +507,9 @@ def main():
             us_data.append(flights.loc[start_index:end_index].copy())  
             ## OJO: loc->extremo superior inclusivo, iloc->extremo superior exclusivo
 
-        print(f' Processing: {date.strftime("%Y-%m-%d")} ({len(us_data)} tray) '.center(56,'='))
+        print(f' Processing: {date.strftime("%Y-%m-%d")} ({len(us_data)} tray) '.center(60,'='))
 
-        with concurrent.futures.ProcessPoolExecutor(max_workers=7) as executor:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
             result = list(executor.map(fix_trajectory, us_data))
 
         if len(result) > 0:
